5e1f52e37293b24703b8b2b48753535b7a8c7936
iradukun <mustaphairadukunda@gmail.com>
2024-12-10 19:22:05 +0200

    fix: deleted un used codes

--- src/components/ui/button.tsx
+++ src/components/ui/button.tsx
@@ -39,12 +39,6 @@ export interface ButtonProps
     VariantProps<typeof buttonVariants> {
   asChild?: boolean
 }
-interface ButtonContainer extends  React.ButtonHTMLAttributes<HTMLButtonElement>,
-VariantProps<typeof buttonVariants>{
-  asChild?: boolean
-}
-
-
 const Button = React.forwardRef<HTMLButtonElement, ButtonProps>(
   ({ className, variant, size, asChild = false, ...props }, ref) => {
     const Comp = asChild ? Slot : "button"
